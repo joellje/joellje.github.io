fde9a3d7bc4b2900a0a7a8061d74e26f27e3243a
Joel Lim <joellimjieen@hotmail.com>
2023-02-08 09:29:47 -0800

    changed footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,10 +4,10 @@ import { BsFillArrowUpCircleFill } from "react-icons/bs";
 export default function Footer() {
   return (
     <div className="flex flex-col items-center justify-center text-white">
-      <div className="font-bold text-sm sm:text-xl ">
+      <div className="font-bold text-xs sm:text-base ">
         Made with ReactJS and TailwindCSS
       </div>
-      <div className="mt-1 mb-1 text-sm sm:text-xl ">© Joel Lim 2023</div>
+      <div className="mt-1 mb-1 text-xs sm:text-base ">© Joel Lim 2023</div>
       <BsFillArrowUpCircleFill
         className="absolute right-5 bottom-100 text-2xl sm:text-3xl hover:scale-125 transition-all duration-200 ease-linear cursor-pointer sm:hover:text-orange-600"
         onClick={() =>
